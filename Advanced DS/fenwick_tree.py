f8707f9787bddd9995afc3416a67119d5287c0ee
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2025-09-01 21:42:29 +0530

    Create fenwick_tree.py

---
+++ Advanced DS/fenwick_tree.py
@@ -0,0 +1,21 @@
+def sum(i):
+	res = 0
+	while i>0:
+		res += t[i]
+		i -= (i&-i)
+	return res
+
+def update(i,x):
+	while i<=n:
+		t[i] += x
+		i += (i&-i)
+
+n = len(a)
+a = [int(i) for i in input().split()]
+N = n
+t = [0]*(n+1)
+for i in range(n): t[i+1] = a[i]
+for i in range(1, n):
+	p = i + (i&-i)
+	if p<=n:
+		tree[p] = tree[p] + tree[i]
