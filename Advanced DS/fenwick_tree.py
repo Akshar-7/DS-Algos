2c1c93f408ed4b16eead5e55efa1c8e99f2a2673
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2025-09-05 10:27:05 +0530

    Update fenwick_tree.py

--- Advanced DS/fenwick_tree.py
+++ Advanced DS/fenwick_tree.py
@@ -1,20 +1,20 @@
-def sum(i):
-	res = 0
-	while i>0:
-		res += t[i]
-		i -= (i&-i)
-	return res
-
-def update(i,x):
-	while i<=n:
-		t[i] += x
-		i += (i&-i)
-
-n = len(a)
-a = [int(i) for i in input().split()]
-t = [0]*(n+1)
-for i in range(n): t[i+1] = a[i]
-for i in range(1, n):
-	p = i + (i&-i)
-	if p<=n:
-		t[p] = t[p] + t[i]
+	def sum(i):
+		res = 0
+		while i>0:
+			res += t[i]
+			i -= (i&-i)
+		return res
+	
+	def update(i,x):
+		while i<=n:
+			t[i] += x
+			i += (i&-i)
+	
+	n = len(a)
+	a = [int(i) for i in input().split()]
+	t = [0]*(n+1)
+	for i in range(n): t[i+1] = a[i]
+	for i in range(1, n):
+		p = i + (i&-i)
+		if p<=n:
+			t[p] = t[p] + t[i]
