1756113096c8b27255c37fa543dadf4246ff37c0
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2025-09-05 10:25:40 +0530

    Update segment_tree.py

--- Advanced DS/segment_tree.py
+++ Advanced DS/segment_tree.py
@@ -1,27 +1,27 @@
-def update(i, v):
-  i +=N
-  t[i] = v
-  while i>1:
-    i //=2
+  def update(i, v):
+    i +=N
+    t[i] = v
+    while i>1:
+      i //=2
+      t[i] = max(t[2*i], t[2*i+1])
+  
+  def query(l, r):
+    l +=N; r +=N
+    ans = 0
+    while l<r:
+      if l&1==1:
+        ans = max(ans, t[l])
+        l +=1
+      if r&1==1:
+        r-=1
+        ans = max(ans, t[r])
+      l>>=1; r>>=1
+    return ans
+  
+  N = 1
+  while N<n: N *=2  # N= 2^x & N>= n
+  t = [0]*2*N
+  for i in range(n):
+    t[N+i] = a[i]
+  for i in range(N-1, 0, -1):
     t[i] = max(t[2*i], t[2*i+1])
-
-def query(l, r):
-  l +=N; r +=N
-  ans = 0
-  while l<r:
-    if l&1==1:
-      ans = max(ans, t[l])
-      l +=1
-    if r&1==1:
-      r-=1
-      ans = max(ans, t[r])
-    l>>=1; r>>=1
-  return ans
-
-N = 1
-while N<n: N *=2  # N= 2^x & N>= n
-t = [0]*2*N
-for i in range(n):
-  t[N+i] = a[i]
-for i in range(N-1, 0, -1):
-  t[i] = max(t[2*i], t[2*i+1])
