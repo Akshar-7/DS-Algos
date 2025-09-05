5dbe74439ad9c4cd3877c0d9b17d2c3706385d5d
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2025-09-05 10:28:38 +0530

    Update dijkstra.py

--- Graphs/dijkstra.py
+++ Graphs/dijkstra.py
@@ -1,11 +1,11 @@
 import heapq
-def djk(x):
-  d = [10*18]*(n+1); d[x]=0
-  h = [(0,x)]
-  while h:
-    dx,x = heapq.heappop(h)
-    if dx!=d[x]: continue
-    for v,w in g[x]:
-      if d[v]>d[x]+w:
-        d[v]=d[x]+w
-        heapq.heappush(h, (d[v],v))
+  def djk(x):
+    d = [10*18]*(n+1); d[x]=0
+    h = [(0,x)]
+    while h:
+      dx,x = heapq.heappop(h)
+      if dx!=d[x]: continue
+      for v,w in g[x]:
+        if d[v]>d[x]+w:
+          d[v]=d[x]+w
+          heapq.heappush(h, (d[v],v))
