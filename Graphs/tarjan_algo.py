b127dd8076f7eb45b91b8f2cd0959d6b292c4165
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2025-09-05 10:32:14 +0530

    Update tarjan_algo.py

--- Graphs/tarjan_algo.py
+++ Graphs/tarjan_algo.py
@@ -1,29 +1,29 @@
-def dfs(x,p):
-  global t
-  stack.append(x); instack[x]=1
-  tt[x] = low[x] = t
-  t+=1
-  for v in g[x]:
-    if tt[v]==-1:
-      dfs(v,x)
-      low[x] = min(low[x], low[v])
-    elif instack[v]:
-      low[x] = min(low[x], tt[v])
-  if low[x]==tt[x]:
-    scc = []
-    while stack:
-      y = stack.pop()
-      scc.append(y); instack[y]=0
-      if y==x: break
-    sccs.append(scc)
-
-t = 0
-sccs = []
-tt = [-1]*(n+1)
-low = [-1]*(n+1)
-instack = [0]*(n+1)
-for i in range(1,n+1):
-  stack = []
-  if tt[i]==-1:
-    dfs(i,0)
-print(sccs)
+  def dfs(x,p):
+    global t
+    stack.append(x); instack[x]=1
+    tt[x] = low[x] = t
+    t+=1
+    for v in g[x]:
+      if tt[v]==-1:
+        dfs(v,x)
+        low[x] = min(low[x], low[v])
+      elif instack[v]:
+        low[x] = min(low[x], tt[v])
+    if low[x]==tt[x]:
+      scc = []
+      while stack:
+        y = stack.pop()
+        scc.append(y); instack[y]=0
+        if y==x: break
+      sccs.append(scc)
+  
+  t = 0
+  sccs = []
+  tt = [-1]*(n+1)
+  low = [-1]*(n+1)
+  instack = [0]*(n+1)
+  for i in range(1,n+1):
+    stack = []
+    if tt[i]==-1:
+      dfs(i,0)
+  print(sccs)
