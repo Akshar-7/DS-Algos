590b9f66bc0062c6b850ac47f477d44c377a4798
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2025-09-05 10:28:59 +0530

    Update bipartite.py

--- Graphs/bipartite.py
+++ Graphs/bipartite.py
@@ -1,15 +1,15 @@
 from collections import deque
-def bipartite(sx):
-    s = deque([sx]); vis[sx]=1; clr[sx]=0
-    while s:
-      x = s.popleft()
-      for v in g[x]:
-        if vis[v]:
-          if clr[v]==clr[x]: return False
-          continue
-        vis[v]=1; clr[v] = clr[x]^1
-        s.append(v)
-    return True
-
-clr = [-1]*(n+1)
-vis = [0]*(n+1)
+    def bipartite(sx):
+        s = deque([sx]); vis[sx]=1; clr[sx]=0
+        while s:
+          x = s.popleft()
+          for v in g[x]:
+            if vis[v]:
+              if clr[v]==clr[x]: return False
+              continue
+            vis[v]=1; clr[v] = clr[x]^1
+            s.append(v)
+        return True
+    
+    clr = [-1]*(n+1)
+    vis = [0]*(n+1)
