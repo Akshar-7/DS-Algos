549b22a65825c47c88110a0c34b02b3de570bea8
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2025-10-04 10:18:43 +0530

    Create euler-phi.py

---
+++ Sieve/euler-phi.py
@@ -0,0 +1,18 @@
+n = 100
+p = []
+pr = [1]*(n+1)
+phi = [0]*(n+1)
+phi[1]=1
+for i in range(2,n):
+  if pr[i]:
+    p.append(i); phi[i] = i-1
+  for x in p:
+    if i*x>n: break
+    pr[i*x] = 0
+    if i%x==0:
+      print(i, i*x, 0)
+      phi[i*x] = phi[i]*x
+      break
+    else:
+      print(i, i*x, 1)
+      phi[i*x] = phi[i]*phi[x]
