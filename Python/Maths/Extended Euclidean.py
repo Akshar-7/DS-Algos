b8ab3baaac8363caa6b3771605400dc79d8ece85
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2025-11-05 15:02:26 +0530

    Update Extended Euclidean.py

--- Python/Maths/Extended Euclidean.py
+++ Python/Maths/Extended Euclidean.py
@@ -1,8 +1,7 @@
 def gcd(a, b):
-  if (b == 0):
-    x, y = 0, 1
-    return a,x,y
-  d, x1, y1 = gcd(b, a %b)
+  if b==0:
+    return a,1,0
+  g,x1,y1 = gcd(b, a%b)
   x = y1
-  y = x1 -y1 *(a //b)
-  return d, x, y
+  y = x1 -y1*(a//b)
+  return g,x,y
