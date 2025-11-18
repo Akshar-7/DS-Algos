614bab2a9044ae9e9de38006eca3dd3653d514e1
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2025-11-19 00:03:53 +0530

    Update Segment Tree.py
    Fix issues with lazy propagation methods in segment tree.

--- Python/Data Structures/Segment Tree.py
+++ Python/Data Structures/Segment Tree.py
@@ -1,5 +1,6 @@
 #1 TODO : Add Lazy Propagation
 #2 Under Construction
+#3 The Added Lazy methods are wrong for iterative. Fix that
 class Node():
   __slots__ = ['x', 'i', 'lazy']
   def __init__(self):
