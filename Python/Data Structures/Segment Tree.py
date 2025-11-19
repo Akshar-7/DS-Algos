fc6e22feb81ff93ada086bd6f1f0b19567a2406b
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2025-11-19 14:52:52 +0530

    Update Segment Tree.py

--- Python/Data Structures/Segment Tree.py
+++ Python/Data Structures/Segment Tree.py
@@ -12,7 +12,7 @@ def copy(a):
   res = assign(a.x, a.i)
   return res
 
-def func(a, b):
+def merge(a, b):
   res = copy(a)
   if a.x > b.x: res = copy(b)
   return res
@@ -22,7 +22,7 @@ def assign(x, i):
   res.x = x
   res.i = i
   return res
-# NOT TESTED
+
 def apply(i, val):
   t[i].x += val
   if i<N: t[i].lazy += val
@@ -36,47 +36,48 @@ def build(a):
   for i in range(n):
     t[N+i] = assign(a[i], i)
   for i in range(N-1, 0, -1):
-    t[i] = func(t[i<<1], t[i<<1 |1])
+    t[i] = merge(t[i<<1], t[i<<1 |1])
   return t
-# Need some work here # [l, r]
+# [l, r)
 def update_rng(t, l, r, v):
   l +=N; r +=N
   while l<r:
     if l&1==1:
-      t[l] = func(t[l], v)
+      apply(l, v)
       l +=1
     if r&1==1:
       r-=1
-      t[r] = func(t[r], v)
+      apply(r, v)
     l>>=1; r>>=1
-# NOT TESTED
-def propagate(t, i):
-  if t[i].lazy != 0 and i < N:
-      t[i<<1].lazy += t[i].lazy
-      t[i<<1 |1].lazy += t[i].lazy
-			t[i].lazy = 0
+
+def push(t, p):
+  for s in range(h, 0, -1):
+    i = p >> s
+    if t[i].lazy != 0:
+      apply(i<<1, t[i].lazy)
+      apply(i<<1|1, t[i].lazy)
+      t[i].lazy = 0
 
 def update(t, i, v):
   i +=N
   t[i] = assign(v, i-N)
   while i>1:
     i //=2
-    t[i] = func(t[i<<1], t[i<<1 |1])
+    t[i] = merge(t[i<<1], t[i<<1 |1])
 # [l, r)
 def query_rng(t, l, r):
   l +=N; r +=N
+  push(t, l); push(t, r-1)  # NEW
   ansL = Node(); ansR = Node()
   while l<r:
     if l&1==1:
-      propagate(t, l)  # NEW
-      ansL = func(ansL, t[l])
+      ansL = merge(ansL, t[l])
       l +=1
     if r&1==1:
       r-=1
-      propagate(t, r)  # NEW
-      ansR = func(t[r], ansR)
+      ansR = merge(t[r], ansR)
     l>>=1; r>>=1
-  return func(ansL, ansR)
+  return merge(ansL, ansR)
 # [l, r]
 def query_idx(t, l, r, k):
   L, R = 0, N-1
