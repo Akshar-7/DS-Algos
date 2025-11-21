0a6a66f5d311428d701abc0ffba7ab7a3eccae7c
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2025-11-21 23:15:44 +0530

    Modify find_kth_seg method to find_seg method to handle custom range queries

--- C++/Data Structures/Segment Tree.cpp
+++ C++/Data Structures/Segment Tree.cpp
@@ -8,7 +8,7 @@ class Seg {
 public:
 	ll n;  vector<node> seg;
 	Seg(ll n) {
-		this->n = n;  seg.resize(4LL * (n + 1LL));
+		this->n = n;  seg.resize(4LL*(n +1LL));
 	}
 	// change here
 	node combine(node &a, node &b) {
@@ -35,8 +35,8 @@ public:
 	void propagate(ll &idx, ll &tl, ll &tr) {
 		if (seg[idx].lazy != LAZY_DEF and tl != tr) {
 			ll mid = tl + ((tr - tl) >> 1LL);
-			update_logic(2 * idx + 1, tl, mid, seg[idx].lazy);
-			update_logic(2 * idx + 2, mid + 1, tr, seg[idx].lazy);
+			update_logic(2*idx +1, tl, mid, seg[idx].lazy);
+			update_logic(2*idx +2, mid+1, tr, seg[idx].lazy);
 			seg[idx].lazy = LAZY_DEF;
 		}
 	}
@@ -46,17 +46,17 @@ public:
 			return;
 		}
 		ll mid = tl + ((tr - tl) >> 1LL);
-		build_seg(2LL * idx + 1LL, tl, mid, a);
-		build_seg(2LL * idx + 2LL, mid + 1, tr, a);
-		seg[idx] = combine(seg[2LL * idx + 1LL], seg[2LL * idx + 2LL]);
+		build_seg(2*idx +1, tl, mid, a);
+		build_seg(2*idx +2, mid+1, tr, a);
+		seg[idx] = combine(seg[2*idx +1], seg[2*idx +2]);
 	}
 	node query_seg(ll idx, ll tl, ll tr, ll &l, ll &r) {
 		propagate(idx, tl, tr);
 		if (tl >= l and tr <= r) return seg[idx];
 		if (tr < l or tl > r) return no_overlap_return();
 		ll mid = tl + ((tr - tl) >> 1LL);
-		node left = query_seg(2LL * idx + 1LL, tl, mid, l, r);
-		node right = query_seg(2LL * idx + 2LL, mid + 1LL, tr, l, r);
+		node left = query_seg(2*idx +1, tl, mid, l, r);
+		node right = query_seg(2*idx +2, mid+1, tr, l, r);
 		return combine(left, right);
 	}
 	void update_seg(ll idx, ll tl, ll tr, ll &l, ll &r, ll &x) {
@@ -67,29 +67,31 @@ public:
 			return;
 		}
 		ll mid = tl + ((tr - tl) >> 1LL);
-		update_seg(2LL * idx + 1LL, tl, mid, l, r, x);
-		update_seg(2LL * idx + 2LL, mid + 1LL, tr, l, r, x);
-		seg[idx] = combine(seg[2LL * idx + 1LL], seg[2LL * idx + 2LL]);
+		update_seg(2*idx +1, tl, mid, l, r, x);
+		update_seg(2*idx +2, mid+1, tr, l, r, x);
+		seg[idx] = combine(seg[2*idx +1], seg[2*idx +2]);
 	}
-	ll find_kth_seg(ll idx, ll tl, ll tr, ll k) {
-        if (tl == tr) return tl;
-        propagate(idx, tl, tr);
-        ll mid = tl + ((tr - tl) >> 1LL);
-        if (seg[2*idx +1].x >= k) return find_kth_seg(2*idx +1, tl, mid, k);
-        return find_kth_seg(2*idx +2, mid+1, tr, k -seg[2*idx +1].x);
-    }
+	int find_seg(ll idx, ll tl, ll tr, ll &l, ll &r, ll k) {
+		if (tl > r or tr < l) return -1;
+		if (seg[idx].x < k) return -1;
+	    if (tl == tr) return tl;
+	    propagate(idx, tl, tr);
+	    ll mid = tl + ((tr - tl) >> 1LL);
+	    ll res = find_seg(2*idx +1, tl, mid, l, r, k);
+	    if (res != -1) return res;
+	    return find_seg(2*idx +2, mid+1, tr, l, r, k - seg[2*idx +1].x);
+	}
 
 	void build(vector<ll> &a) {
-		build_seg(0, 0, n - 1, a);
+		build_seg(0, 0, n-1, a);
 	}
 	node query(ll l, ll r) {
-		return query_seg(0, 0, n - 1, l, r);
+		return query_seg(0, 0, n-1, l, r);
 	}
 	void update(ll l, ll r, ll val) {
-		update_seg(0, 0, n - 1, l, r, val);
+		update_seg(0, 0, n-1, l, r, val);
 	}
-	int find_kth(ll k) {
-        if (seg[0].x < k) return -1; 
-        return find_kth_seg(0, 0, n - 1, k);
-    }
+	int find(ll l, ll r, ll k) {
+    	return find_seg(0, 0, n-1, l, r, k);
+  	}
 };
