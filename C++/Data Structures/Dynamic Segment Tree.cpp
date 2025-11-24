948d7fed6b49b9c9e4a0a52611fd198fa2bdbb52
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2025-11-24 11:06:14 +0530

    Refactor Dynamic Segment Tree to use int instead of ll

--- C++/Data Structures/Dynamic Segment Tree.cpp
+++ C++/Data Structures/Dynamic Segment Tree.cpp
@@ -2,14 +2,14 @@ using ll = long long;
 const ll LAZY_DEF = 0; 
 
 struct node {
-    ll sum = 0;  ll mxp = 0;  ll lazy = LAZY_DEF;  bool has_lazy = false;  int l = -1;  int r = -1;
+    ll sum = 0;  ll lazy = LAZY_DEF;  bool has_lazy = false;  int l = -1;  int r = -1;
 };
 
 class DynamicSeg {
 public:
-    ll n;
+    int n;
     vector<node> seg;
-    DynamicSeg(ll n) {
+    DynamicSeg(int n) {
         this->n = n;
         seg.reserve(5000000); // Pre-allocate memory
         create_node(); // Dummy root at i=0
@@ -18,7 +18,7 @@ public:
         seg.push_back(node());
         return seg.size() - 1;
     }
-    void clear(ll new_n) {
+    void clear(int new_n) {
         n = new_n;
         seg.clear();
         create_node();
@@ -34,18 +34,15 @@ public:
     node combine(node &a, node &b) {
         node res;
         res.sum = a.sum + b.sum;
-        res.mxp = max(a.mxp, a.sum + b.mxp);
         return res;
     }
     // change here
-    void update_logic(int idx, ll tl, ll tr, ll val) {
+    void update_logic(int idx, int tl, int tr, int val) {
         seg[idx].lazy = val;
         seg[idx].has_lazy = true;
         seg[idx].sum = val * (tr - tl + 1);
-        seg[idx].mxp = (val > 0) ? seg[idx].sum : 0;
     }
-
-    void propagate(int idx, ll tl, ll tr) {
+    void propagate(int idx, int tl, int tr) {
         if (seg[idx].has_lazy && tl != tr) {
             extend(idx);
             ll mid = tl + ((tr - tl) >> 1LL);
@@ -56,7 +53,7 @@ public:
         }
     }
 
-    void update_seg(int idx, ll tl, ll tr, ll &l, ll &r, ll &val) {
+    void update_seg(int idx, int tl, int tr, int &l, int &r, int &val) {
         if (tl > r or tr < l) return;
         
         if (tl >= l and tr <= r) {
@@ -65,26 +62,23 @@ public:
         }
         propagate(idx, tl, tr);
         extend(idx);
-        ll mid = tl + ((tr - tl) >> 1LL);
+        int mid = tl + ((tr - tl) >> 1LL);
         update_seg(seg[idx].l, tl, mid, l, r, val);
         update_seg(seg[idx].r, mid + 1, tr, l, r, val);
         node res = combine(seg[seg[idx].l], seg[seg[idx].r]);
         seg[idx].sum = res.sum;
-        seg[idx].mxp = res.mxp;
     }
-
     // Merges tree rooted at 'b' into tree rooted at 'a' and return idx of new root
-    int merge(int a, int b, ll tl, ll tr) {
+    int merge(int a, int b, int tl, int tr) {
         if (a == -1 || b == -1) return (a != -1) ? a : b;
         if (tl == tr) {
             seg[a].sum += seg[b].sum;
-            seg[a].mxp = (seg[a].sum > 0) ? seg[a].sum : 0;
             return a;
         }
         // Push lazy down before merging children to ensure consistency
         propagate(a, tl, tr);
         propagate(b, tl, tr);
-        ll mid = tl + ((tr - tl) >> 1LL);
+        int mid = tl + ((tr - tl) >> 1LL);
         seg[a].l = merge(seg[a].l, seg[b].l, tl, mid);
         seg[a].r = merge(seg[a].r, seg[b].r, mid + 1, tr);
         // Pull up
@@ -92,15 +86,13 @@ public:
         node right = (seg[a].r != -1) ? seg[seg[a].r] : no_overlap_return();
         node res = combine(left, right);
         seg[a].sum = res.sum;
-        seg[a].mxp = res.mxp;
         return a;
     }
-
-    node query_seg(int idx, ll tl, ll tr, ll &l, ll &r) {
+    node query_seg(int idx, int tl, int tr, int &l, int &r) {
         propagate(idx, tl, tr);
         if (tl >= l and tr <= r) return seg[idx];
         if (tr < l or tl > r) return no_overlap_return();
-        ll mid = tl + ((tr - tl) >> 1LL);
+        int mid = tl + ((tr - tl) >> 1LL);
         node left, right;
         if (seg[idx].l != -1) left = query_seg(seg[idx].l, tl, mid, l, r);
         else left = no_overlap_return();
@@ -108,24 +100,23 @@ public:
         else right = no_overlap_return();
         return combine(left, right);
     }
-
-    int find_seg(int idx, ll tl, ll tr, ll h, ll ch) {
+    int find_seg(int idx, int tl, int tr, int h, int ch) {
         if (ch + seg[idx].mxp <= h)  return (tr - tl + 1);
         if (tl == tr) return 0;
         propagate(idx, tl, tr);
         extend(idx);
         ll mid = tl + ((tr - tl) >> 1LL);
-        if (current_h + seg[seg[idx].l].mxp > h)  return find_seg(seg[idx].l, tl, mid, h, current_h);
+        if (ch + seg[seg[idx].l].mxp > h)  return find_seg(seg[idx].l, tl, mid, h, ch);
         return (mid - tl + 1) + find_seg(seg[idx].r, mid+1, tr, h, ch +seg[seg[idx].l].sum);
     }
 
-    void update(ll l, ll r, ll val) {
+    void update(int l, int r, int val) {
         update_seg(0, 1, n, l, r, val);
     }
-    node query(ll l, ll r) {
+    node query(int l, int r) {
         return query_seg(0, 1, n, l, r);
     }
-    int find(ll h) {
+    int find(int h) {
         return find_seg(0, 1, n, h, 0);
     }
 };
