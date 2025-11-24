d0f6c2977658f979ee3f67894a996ef42fc31a4c
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2025-11-24 20:16:54 +0530

    Update Co-ordinate Compression.cpp

--- C++/Add Ons/Co-ordinate Compression.cpp
+++ C++/Add Ons/Co-ordinate Compression.cpp
@@ -1,3 +1,15 @@
+// Locally
+
+vector<int> d = a;
+sort(d.begin(), d.end());
+d.resize(unique(d.begin(), d.end()) - d.begin());
+for (int i = 0; i < n; ++i) {
+  a[i] = lower_bound(d.begin(), d.end(), a[i]) - d.begin();
+}
+
+
+// Function
+
 map<int, int> cocom(vector<int> &b){
     sort(b.begin(), b.end());
     map<int, int> m;
