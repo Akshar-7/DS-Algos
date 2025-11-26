e557554191beb139a75b2ea4caaefcbcac69d0e8
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2025-11-26 15:56:30 +0530

    Changed implementation of build and added kth_element query

--- C++/Data Structures/Fenwick Tree.cpp
+++ C++/Data Structures/Fenwick Tree.cpp
@@ -1,22 +1,37 @@
 class Fenwick {
 public:
-	int n;  vector<ll> fw;
+	int n;  vector<ll> t; int LOGN;
 	Fenwick_Tree(int sz) {
 		this -> n = sz;
-		fw.assign(n + 5, 0);
+		this -> LOGN = 31 - __builtin_clz(sz);
+		t.assign(n + 5, 0);
 	};
 	void build(vector<ll> &a) {
-		for (ll i = 1; i <= n; i++)  add(i, a[i]);
+	    for (int i = 1; i <= n; i++) {
+			t[i] += a[i-1];
+	        int par = i + (i & -i);
+	        if (par <= n) t[par] += t[i];
+	    }
+	}
+	int kth_element(int k) {
+	    int idx = 0;
+	    for (int i = 1 << LOGN; i > 0; i >>= 1) {
+	        if (idx + i <= n && tree[idx + i] < k) {
+	            idx += i;
+	            k -= tree[idx];
+	        }
+	    }
+    	return idx + 1;
 	}
 	ll make_query(ll idx) {
 		ll res = 0;
-		for (; idx > 0; idx -= (idx & (-idx)))  res += fw[idx];
+		for (; idx > 0; idx -= (idx & (-idx)))  res += t[idx];
 		return res;
 	}
 	ll query(ll l, ll r) {
 		return make_query(r) - make_query(l - 1);
 	}
 	void add(ll idx, ll x) {
-		for (; idx <= n; idx += (idx & (-idx)))  fw[idx] += x;
+		for (; idx <= n; idx += (idx & (-idx)))  t[idx] += x;
 	}
 };
