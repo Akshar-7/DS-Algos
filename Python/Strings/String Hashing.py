36fa0e5769b3e670e5c751520bbb75a40073c787
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2026-01-09 20:56:49 +0530

    Convert into insert function

--- Python/Strings/String Hashing.py
+++ Python/Strings/String Hashing.py
@@ -1,13 +1,16 @@
-f = {}
 z = ord('a')
 mod = [10**9 +7, 10**9 +9]
-w = [29, 31]
-k = [1]*2
-hsh = [0]*2
-for x in s:
-  y = ord(x) -z
-  for i in range(2):
-    hsh[i] = (hsh[i] + k[i]*y %mod[i])
-    k[i] = k[i] *w[i] %mod[i]
-    tup = (hsh[0], hsh[1])
-    f[tup] = f.get(tup, 0) +1
+k = [29, 31]
+
+def insert(s):
+  w = [1]*2
+  hsh = [0]*2
+  for x in s:
+    y = ord(x) -z
+    for i in range(2):
+      hsh[i] = (hsh[i] + y*w[i]) %mod[i]
+      w[i] = w[i] * k[i] %mod[i]
+      tup = (hsh[0], hsh[1])
+      f[tup] = f.get(tup, 0) +1
+
+f = {}
