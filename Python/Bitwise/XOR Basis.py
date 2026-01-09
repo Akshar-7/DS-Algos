c34c68fb3e905b3c028b5223e01471945ab759a6
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2026-01-09 23:01:00 +0530

    Added mask to find original values of the basis vectors

--- Python/Bitwise/XOR Basis.py
+++ Python/Bitwise/XOR Basis.py
@@ -1,15 +1,20 @@
 lg = 32
 def insert(x, j):
+  #mask = 0
   for i in range(lg-1, -1, -1):
     if x&1<<i==0: continue
     if b[i]==0:
       b[i] = x; d[i] = j
+      #m[i] = mask^(1<<i)
       break
+    #mask ^= m[i]
     x ^= b[i]
 
 def query(x):
+  #mask = 0
   for i in range(lg-1, -1, -1):
     if x&1<<i==0: continue
     x ^= b[i]
+    #mask ^= m[i]
   return x==0
 
