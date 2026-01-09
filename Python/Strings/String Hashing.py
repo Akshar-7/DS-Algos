be1616b8d9bcb28a4d6580b4712441a214926c4c
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2026-01-09 21:09:32 +0530

    Refactor string hashing functions
    Refactor insert function and add count function for string hashing.

--- Python/Strings/String Hashing.py
+++ Python/Strings/String Hashing.py
@@ -3,14 +3,24 @@ mod = [10**9 +7, 10**9 +9]
 k = [29, 31]
 
 def insert(s):
-  w = [1]*2
-  hsh = [0]*2
+  w = [1]*2; hsh = [0]*2
   for x in s:
-    y = ord(x) -z
+    y = ord(x) -z +1
     for i in range(2):
       hsh[i] = (hsh[i] + y*w[i]) %mod[i]
       w[i] = w[i] * k[i] %mod[i]
       tup = (hsh[0], hsh[1])
       f[tup] = f.get(tup, 0) +1
+
+def count(s):
+  w = [1]*2; hsh = [0]*2; res = 0
+  for x in s:
+    y = ord(x) -z +1
+    for i in range(2):
+      hsh[i] = (hsh[i] + y*w[i]) %mod[i]
+      w[i] = w[i] * k[i] %mod[i]
+      tup = (hsh[0], hsh[1])
+      res += f.get(tup, 0)
+  return res
 
 f = {}
