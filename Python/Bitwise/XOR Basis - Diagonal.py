cdbeda991d453a94c03ad4b5f48a283f3eefe714
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2026-01-10 22:43:14 +0530

    Implement Diagonal XOR basis
    Added functions to manage independent vectors and queries.

---
+++ Python/Bitwise/XOR Basis - Diagonal.py
@@ -0,0 +1,27 @@
+# Independent Vectors
+# Basis does not depend on insertion order
+# Maintains minimum basis
+# Check equality of basis
+# Simple k-th queries
+# Not good for merging / modification
+
+sz = 0
+def insert(x):
+  for y in b:
+    x = min(x, x^y)
+  for i in range(sz):
+    b[i] = min(b[i], b[i]^x)
+  if x==0: return False
+  b.append(x); sz+=1
+  for i in range(sz-1,0,-1):
+    if b[i]<b[i-1]:
+      b[i],b[i-1] = b[i-1],b[i]
+  return True
+
+def lexk(k):
+  k-=1  # k starts from 1
+  ans = 0
+  for i in range(sz):
+    if k&1<<i:
+      ans ^= b[i]
+  return ans
