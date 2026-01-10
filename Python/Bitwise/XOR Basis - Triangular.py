b5b81317558423b25c02f109c9c203553fca5c7e
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2026-01-10 22:50:22 +0530

    Implement lexk function for XOR basis
    Add lexk function to compute lexicographical k-th xor.

--- Python/Bitwise/XOR Basis - Triangular.py
+++ Python/Bitwise/XOR Basis - Triangular.py
@@ -18,3 +18,14 @@ def query(x):
     #mask ^= m[i]
   return x==0
 
+def lexk(k):
+  res = 0
+  ttl = 1<<len(b)
+  for i in range(lg-1, -1, -1):
+    if b[i]==0: continue
+    ttl >>=1
+    bit = res&1<<i
+    if (k<=ttl and bit) or (k>ttl and not bit):
+      res ^= b[i]
+    if k>ttl: k -=ttl
+  return res
