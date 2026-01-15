24225e081d562fea3167b142707cbe11067fe944
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2026-01-15 10:09:17 +0530

    Improve XOR basis functions and add "cntl" function

--- Python/Bitwise/XOR Basis - Triangular.py
+++ Python/Bitwise/XOR Basis - Triangular.py
@@ -7,8 +7,7 @@ def insert(x, j):
       b[i] = x; d[i] = j
       #m[i] = mask^(1<<i)
       return 1
-    #mask ^= m[i]
-    x ^= b[i]
+    x ^= b[i]; #mask ^= m[i]
   return 0
 
 def query(x):
@@ -24,11 +23,10 @@ def maxxor(x):
     if x < x^b[i]:
       x ^= b[i]; #mask ^= m[i]
   return x
-
+# Returns the k-th smallest XOR [1, 2^sz]
 def lexk(k):
-  res = sz = 0
-  for x in b:
-    if x!=0: sz +=1
+  res = 0
+  sz = sum((1*(x>0) for x in b))
   ttl = 1<<sz
   if k>ttl: return -1
   for i in range(lg-1, -1, -1):
@@ -39,3 +37,20 @@ def lexk(k):
       res ^= b[i]
     if k>ttl: k -=ttl
   return res
+# Returns the no of distinct XOR < x
+def cntl(x):
+  ans = res = 0
+  sz = sum((1*(x>0) for x in b))
+  for i in range(lg-1, -1, -1):
+    if b[i]>0:
+      sz -=1
+      if x&1<<i:
+        ans += (1<<sz)
+        if res&1<<i==0: res ^= b[i]
+      elif res&1<<i: res ^= b[i]
+    else:
+      bx = x&1<<i
+      br = res&1<<i
+      if bx > br: return ans + (1<<sz)
+      elif bx != br: return ans
+  return ans
