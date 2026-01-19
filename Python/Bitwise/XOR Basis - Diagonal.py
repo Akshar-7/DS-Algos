6e47ca6615a35627613bc4c87bc0429478f01f79
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2026-01-19 21:20:57 +0530

    Refactor XOR basis functions and add solve method
    Refactor insert for clarity and efficiency. Implement solve function for rank reduction of augmented matrix.

--- Python/Bitwise/XOR Basis - Diagonal.py
+++ Python/Bitwise/XOR Basis - Diagonal.py
@@ -5,29 +5,40 @@
 # Simple k-th queries (in O(1) using Four Russian)
 # Not good for merging / modification
 
-sz = 0
+lg = 31
 def insert(x):
-  global sz
-  for y in b:
-    x = min(x, x^y)
-  for i in range(sz):
-    b[i] = min(b[i], b[i]^x)
+  for y in b: x = min(x, x^y)
+  for i in range(lg): b[i] = min(b[i], b[i]^x)
   if x==0: return 0
-  b.append(x); sz+=1
-  for i in range(sz-1,0,-1):
-    if b[i]<b[i-1]:
-      b[i],b[i-1] = b[i-1],b[i]
-  return 1
+  for i in range(lg-1, -1, -1):
+    if x&1<<i:
+      b[i] = x; return 1
 
-def query(x):
-  for y in b:
-    x = min(x, x^y)
-  return x==0
+def solve(a, aug = 1):
+  """
+  Reduces the col-vector augmented matrix (list of ints) in-place returns the rank.
+  aug : defines whether the matrix a is augmented or not.
+  Time Complexity: O(N * bits).
+  """
+  n = len(a)
+  rank = 0
+  for j in range(lg-1, -1, -1):
+    pivot = -1
+    for i in range(rank, n-aug):
+      if a[i]&1<<j:
+        pivot = i; break
+    if pivot == -1:
+      if a[n-1]&1<<j: return -1
+      continue
+    a[rank], a[pivot] = a[pivot], a[rank]
+    for i in range(n):
+      if i!=rank and a[i]&1<<j:
+        a[i] ^= a[rank]
+    rank +=1
+  return rank
 
 def lexk(k):
-  k-=1  # k starts from 1
-  ans = 0
-  for i in range(sz):
-    if k&1<<i:
-      ans ^= b[i]
+  ans = 0; k-=1  # k starts from 1
+  for i in range(lg):
+    if k&1<<i:  ans ^= b[i]
   return ans
