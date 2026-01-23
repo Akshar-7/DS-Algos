0932e66ce30b4676ddd591859b0eaf9739aa4975
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2026-01-23 18:16:58 +0530

    Fixes for better cache efficiency
    Switched the dimension from NxK to KxN for better cache efficiency.

--- Python/Trees/LCA.py
+++ Python/Trees/LCA.py
@@ -1,7 +1,7 @@
 def find(x, d):
   for i in range(k):
     if d&1<<i:
-      x = p[x][i]
+      x = p[i][x]
   return x
 
 def lca(u,v):
@@ -10,22 +10,22 @@ def lca(u,v):
   v = find(v, d)
   if u==v: return u
   for m in range(k-1,-1,-1):
-    pu, pv = p[u][m], p[v][m]
+    pu, pv = p[m][u], p[m][v]
     if pu!=pv:
       u,v = pu,pv
-  return p[u][0]
+  return p[0][u]
 
 k = 0
 while (1<<k)<=n: k+=1
-p = [[0]*k for i in range(n+1)]
+p = [[0]*(n+1) for i in range(k)]
 lvl = [0]*(n+1)
 s = [(1,0)]
 while s:
   x,par = s.pop()
   lvl[x] = lvl[par]+1
-  p[x][0] = par
+  p[0][x] = par
   for i in range(1,k):
-    p[x][i] = p[p[x][i-1]][i-1]
+    p[i][x] = p[i-1][p[i-1][x]]
   for v in g[x]:
     if v==par: continue
     s.append((v,x))
