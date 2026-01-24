65e40e07e2a9bebfb87162485e9f628f776e6ea0
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2026-01-24 18:37:50 +0530

    Implement Graham Scan algorithm for convex hull

---
+++ Python/Maths/Convex Hull/Graham Scan.py
@@ -0,0 +1,29 @@
+import math
+
+def dist(p1, p2):
+  x1, y1, x2, y2 = *p1, *p2
+  return math.sqrt((y2-y1)**2 + (x2-x1)**2)
+
+def polar_angle(p1, p2):
+  if p1[1] == p2[1]:
+    return -math.pi
+  dy = p1[1]-p2[1]
+  dx = p1[0]-p2[0]
+  return math.atan2(dy, dx)
+
+def orientation(p1, p2, p3):
+    x1, y1, x2, y2, x3, y3 = *p1, *p2, *p3
+    d = (y3-y2)*(x2-x1) - (y2-y1)*(x3-x2)
+    if d>0: return 1
+    elif d<0: return -1
+    else: return 0
+
+def graham(points):
+  p0 = min(points, key=lambda p: (p[1], p[0]))
+  points.sort(key=lambda p: (polar_angle(p0, p), dist(p0, p)))
+  hull = []
+  for i in range(len(points)):
+    while len(hull) >= 2 and orientation(hull[-2], hull[-1], points[i]) != 1:
+      hull.pop()
+    hull.append(points[i])
+  return hull
