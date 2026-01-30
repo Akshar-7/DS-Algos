e2a6f3d62af16376b4dfc76a2c890c35a492f7f8
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2026-01-30 20:46:28 +0530

    Refactor Trie implementation with array-based nodes
    To improve speed of the code

--- Python/Data Structures/Trie.py
+++ Python/Data Structures/Trie.py
@@ -1,39 +1,45 @@
-class Node:
-  def __init__(self):
-    self.c = 0
-    self.child = [None]*2
+# Max nodes calculation: (N * bits) + 1
+lg = 30
+BITS = tuple(range(lg-1, -1, -1))
+MAX_NODES = n*lg +1
+# child[node * 2 + bit] stores the index of the next node
+child = [0] * (MAX_NODES * 2)
+count = [0] * MAX_NODES
+nodes_cnt = 1
+def insert(x):
+  nonlocal nodes_cnt
+  nd = 0
+  for i in BITS:
+    bit = (x>>i) &1
+    idx = (nd<<1) |bit
+    if not child[idx]:
+      child[idx] = nodes_cnt
+      nodes_cnt += 1
+    nd = child[idx]
+    count[nd] += 1
 
-def merge(x, root):
-  nd = root
-  for i in range(29, -1, -1):
-    bit = (x>>i)&1
-    if nd.child[bit]==None:
-      nd.child[bit] = Node()
-    chd = nd.child[bit]
-    chd.c +=1
-    nd = chd
+def remove(x):
+  nd = 0
+  for i in BITS:
+    bit = (x>>i) &1
+    idx = (nd<<1) |bit
+    prev = nd
+    nd = child[idx]
+    count[nd] -= 1
+    if count[nd] == 0:
+      child[(prev<<1) |bit] = 0
+      return
 
-def remove(x, root):
-  nd = root
-  for i in range(29, -1, -1):
-    bit = (x>>i)&1
-    chd = nd.child[bit]
-    chd.c -=1
-    if chd.c==0:
-      nd.child[bit] = None
-      del chd
-      break
-    nd = chd
-
-def query(x, root):
-  nd = root
+def query_max(x):
+  nd = 0
   res = 0
-  for i in range(29, -1, -1):
-    bit = (x>>i)&1
-    if nd.child[bit]:
-      res ^=(bit<<i)
-      nd = nd.child[bit]
+  for i in BITS:
+    bit = (x>>i) &1
+    bit ^=1
+    idx = (nd<<1) |bit
+    if child[idx] and count[child[idx]]>0:
+      res |= (1<<i)
+      nd = child[idx]
     else:
-      res ^=((bit^1)<<i)
-      nd = nd.child[bit^1]
+      nd = child[idx^1]
   return res
