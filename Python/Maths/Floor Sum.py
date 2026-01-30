69e0ba7b1d77953831d6227d34e0d5aa868d910f
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2026-01-30 22:38:02 +0530

    Implement generalized floor sum function
    Added a function to compute the generalized floor sum.

--- Python/Maths/Floor Sum.py
+++
@@ -1,8 +0,0 @@
-# Sum of [n/1] + [n/2] + [n/3] + .... + [n/n]
-x = 1
-sm = 0
-while x<=n:
-	y = n//x
-	z = n//y
-	sm += (z-x+1)*y
-	x = z+1
---
+++ Python/Maths/Generalized Floor Sum.py
@@ -0,0 +1,9 @@
+# Summation of floor((a*i + b)/m) from 0 to n-1
+def floor_sum(n, m, a, b):
+  a1, a2 = a //m, a %m
+  s = n * (n-1) //2 *a1
+  b1, b2 = b //m, b %m
+  if a2==0:
+    return s + b1 * n
+  k = (a2 *(n-1) +b2) //m
+  return s +n *(k +b1) -floor_sum(k, a2, m, m +a2 -b2 -1)
