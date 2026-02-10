7ace57fcc1a22ee3820ecdcac7cad40e5d121c32
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2026-02-11 00:57:13 +0530

    Refactor Recursive Segment Tree structure and methods
    Refactor segment tree implementation to use arrays and improve logic.

--- Python/Data Structures/Recursive Segment Tree.py
+++ Python/Data Structures/Recursive Segment Tree.py
@@ -1,78 +1,84 @@
-class node:
-  x = 10**18
-  i = 0; lazy = 0
+from array import array
+LAZY_DEF = 0
+INF = 10**18
+n = 0
+x = idx = lazy = []
+# CHANGE HERE
+def init_seg(sz):   # Initialization
+  global n, x, idx, lazy
+  n = sz
+  x = array('q', [INF]) *(4*n)
+  idx = array('i', [-1]) *(4*n)
+  lazy = array('q', [LAZY_DEF]) *(4*n)
+# CHANGE HERE
+def combine(v1, i1, v2, i2):   # Used in query and pull
+  if v1 <= v2:
+    return (v1, i1)
+  else:
+    return (v2, i2)
+# CHANGE HERE
+def pull(node):   # Used in build & update
+  L = node<<1 |1
+  R = node+1 <<1
+  x[node], idx[node] = combine(x[L], idx[L], x[R], idx[R])
+# CHANGE HERE
+def update_logic(node, tl, tr, val):   # Used in update
+  x[node] += val
+  lazy[node] += val
 
-class Seg:
-	def __init__(self, n):
-	  self.n = n
-	  self.seg = [node()]*(4*n+1)
-	
-	# change here
-	def combine(a, b):
-		res = a
-		if (b.x < res.x): res=b
-		return res
+def no_overlap_return():   # Used in query
+  return (INF, -1)
 
-	# change here
-	def assign(x, idx):
-		res = node()
-		res.x = x
-		res.idx = idx
-		return res
+def propagate(node, tl, tr):   # Used in update & query
+  if lazy[node] != LAZY_DEF and tl != tr:
+    mid = tl + ((tr-tl)>>1)
+    update_logic(node<<1 |1, tl, mid, lazy[node])
+    update_logic(node+1 <<1, mid+1, tr, lazy[node])
+    lazy[node] = LAZY_DEF
 
-	# change here
-	def no_overlap_return():
-		res = node();  res.x = 10**18;
-		return res
+def build_seg(node, tl, tr, a):
+  if tl == tr:
+    x[node], idx[node], lazy[node] = (a[tl], tl, LAZY_DEF)
+    return
+  mid = tl + ((tr-tl) >> 1)
+  build_seg(node<<1 |1, tl, mid, a)
+  build_seg(node+1 <<1, mid+1, tr, a)
+  pull(node)
 
-	# change here
-	def update_logic(idx, tl, tr, val):
-		seg[idx].lazy += val
+def query_seg(node, tl, tr, l, r):
+  propagate(node, tl, tr)
+  if tl >= l and tr <= r:
+    return (x[node], idx[node])
+  if tr < l or tl > r:
+    return no_overlap_return()
+  mid = tl + ((tr-tl)>>1)
+  L = query_seg(node<<1 |1, tl, mid, l, r)
+  R = query_seg(node+1 <<1, mid+1, tr, l, r)
+  # Paste pull logic here & add return values
+  return combine(L[0], L[1], R[0], R[1])
 
-	# change here
-	def propagate(idx, tl, tr):
-		if (seg[idx].lazy != 0):
-			seg[idx].x += seg[idx].lazy
-			if (tl != tr):
-				seg[2 * idx + 1].lazy += seg[idx].lazy
-				seg[2 * idx + 2].lazy += seg[idx].lazy
-			seg[idx].lazy = 0
+def update_seg(node, tl, tr, l, r, val):
+  propagate(node, tl, tr)
+  if tl>r or tr<l: return 
+  if tl >= l and tr <= r:
+    update_logic(node, tl, tr, val)
+    return
+  mid = tl + ((tr-tl)>>1)
+  update_seg(node<<1 |1, tl, mid, l, r, val)
+  update_seg(node+1 <<1, mid+1, tr, l, r, val)
+  pull(node)
 
-	def build_seg(idx, tl, tr, a):
-		if (tl == tr):
-			seg[idx] = assign(a[tl], tl);
-			return;
-		mid = tl + ((tr - tl) >> 1)
-		build_seg(2 * idx + 1, tl, mid, a)
-		build_seg(2 * idx + 2, mid + 1, tr, a)
-		seg[idx] = combine(seg[2 * idx + 1], seg[2 * idx + 2])
+def find_seg(node, tl, tr, l, r, k):
+  if tl > r or tr < l: return -1
+  if x[node] < k: return -1
+  if tl == tr: return tl
+  propagate(node, tl, tr)
+  mid = tl + ((tr-tl)>>1)
+  res = find_seg(node<<1 |1, tl, mid, l, r, k)
+  if res != -1: return res
+  return find_seg(node+1 <<1, mid+1, tr, l, r, k - x[node<<1 |1])
 
-	def query_seg(idx, tl, tr, l, r):
-		propagate(idx, tl, tr)
-		if (tl >= l and tr <= r): return seg[idx]
-		if (tr < l or tl > r): return no_overlap_return()
-		mid = tl + ((tr - tl) >> 1)
-		left = query_seg(2 * idx + 1, tl, mid, l, r)
-		right = query_seg(2 * idx + 2, mid + 1, tr, l, r)
-		return combine(left, right)
-
-	def update_seg(idx, tl, tr, l, r, x):
-		propagate(idx, tl, tr)
-		if (tl > r or tr < l): return;
-		if (tl >= l and tr <= r):
-			update_logic(idx, tl, tr, x)
-			propagate(idx, tl, tr)
-			return;
-		mid = tl + ((tr - tl) >> 1);
-		update_seg(2 * idx + 1, tl, mid, l, r, x)
-		update_seg(2 * idx + 2, mid + 1, tr, l, r, x)
-		seg[idx] = combine(seg[2 * idx + 1], seg[2 * idx + 2])
-
-	def build(a):
-		build_seg(0, 0, n - 1, a)
-
-	def query(l, r):
-		return query_seg(0, 0, n - 1, l, r)
-
-	def update(l, r, val):
-		update_seg(0, 0, n - 1, l, r, val)
+def build(a): build_seg(0, 0, n-1, a)
+def query(l, r): return query_seg(0, 0, n-1, l, r)
+def update(l, r, val): update_seg(0, 0, n-1, l, r, val)
+def find(l, r, k): return find_seg(0, 0, n-1, l, r, k)
