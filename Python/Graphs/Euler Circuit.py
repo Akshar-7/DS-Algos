72b968bd4cfb2d6292d325b1c419afa78ef8ebca
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2026-02-22 01:16:47 +0530

    Refactor to implement iterative Euler circuit algorithm
    Refactor recursive DFS function to a more structured iterative Euler circuit function.

--- Python/Graphs/Euler Circuit.py
+++ Python/Graphs/Euler Circuit.py
@@ -1,21 +1,12 @@
-def dfs(x):
-  while g[x]:
-    v = g[x].pop()
-    g[v].remove(x)
-    yield dfs(v)
-  ans.append(x+1)
-  yield 0
-
-cod = 0
-for x in dg: cod += x&1
-if cod!=0:# and cod!=2:
-  print("IMPOSSIBLE")
-
-st = 0
-for i in range(n):
-  if dg[i]&1:
-    st = i; break
-ans = []
-dfs(st)
-if len(ans)<m+1: print("IMPOSSIBLE")
-else: print(*ans)
+def euler(x):
+    ans = []
+    s = [x]
+    while s:
+      u = s[-1]
+      if g[u]:
+        v,j = g[u].pop()
+        g[v].discard((u,j))
+        s.append(v)
+      else:
+        ans.append(s.pop() +1)
+    return ans
