4ec2d15be0138377f643a39536880fd0840ca17c
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2026-02-22 11:56:05 +0530

    Fix indentation in euler function

--- Python/Graphs/Euler Circuit.py
+++ Python/Graphs/Euler Circuit.py
@@ -1,12 +1,12 @@
 def euler(x):
-    ans = []
-    s = [x]
-    while s:
-      u = s[-1]
-      if g[u]:
-        v,j = g[u].pop()
-        g[v].discard((u,j))
-        s.append(v)
-      else:
-        ans.append(s.pop() +1)
-    return ans
+  ans = []
+  s = [x]
+  while s:
+    u = s[-1]
+    if g[u]:
+      v,j = g[u].pop()
+      g[v].discard((u,j))
+      s.append(v)
+    else:
+      ans.append(s.pop() +1)
+  return ans
