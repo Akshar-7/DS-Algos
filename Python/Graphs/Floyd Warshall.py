2c25c5a0b8908db3d7d0257cf586cf46822a147d
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2026-02-24 10:10:16 +0530

    Fix Floyd-Warshall distance matrix initialization
    Fixed initialization of distance matrix and set diagonal to zero.

--- Python/Graphs/Floyd Warshall.py
+++ Python/Graphs/Floyd Warshall.py
@@ -1,7 +1,6 @@
-d = []
-for i in range(n+1):
-  b = [[10**18]*(n+1) for i in range(n+1)]
-  d.append(b)
+d = [[10**18]*(n+1) for i in range(n+1)]
+for i in range(n+1):  d[i][i]=0
+# Add weights of all edges
 for k in range(1, n+1):
   for i in range(1, n+1):
     for j in range(1, n+1):
