8003143485128fcf0f0508909b96ad5f555ed6a3
Akshat Awasthi <147391847+Akshar-7@users.noreply.github.com>
2026-03-05 21:05:00 +0530

    Renamed modulus variable
    Increases adaptability among different programs

--- Python/Maths/Factorials.py
+++ Python/Maths/Factorials.py
@@ -7,14 +7,14 @@ def pw(x,y,m):
 	return res
 
 N = 10**5+1
-m = 10**9+7
+mod = 10**9+7
 f = [1]*N
 for i in range(2,N):
-	f[i] = f[i-1]*i %m
+	f[i] = f[i-1]*i %mod
 fr = [1]*N
-fr[N-1] = pw(f[N-1], m-2, m)
+fr[N-1] = pw(f[N-1], mod-2, mod)
 for i in range(N-2, 1, -1):
-	fr[i] = fr[i+1]*(i+1) %m
+	fr[i] = fr[i+1]*(i+1) %mod
 
 def comb(n, r):
-	return f[n] *fr[r] *fr[n-r] %m
+	return f[n] *fr[r] *fr[n-r] %mod
